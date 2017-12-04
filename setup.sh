eb30aabcc44fd3920eb82415568238ec2385c01c
Hiromitsu Yamaguchi <yamagame@mac.com>
2017-12-04 18:32:28 +0900

    setupスクリプトを更新

--- setup.sh
+++ setup.sh
@@ -1,10 +1,14 @@
-apt-get update
-apt-get upgrade -y
-apt-get install npm -y
-apt-get install ibus-anthy -y
-npm install npm@latest -g
-npm install n -g
-n latest
+#!/bin/sh
+cd ~
+
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install npm -y
+sudo apt-get install ibus-anthy -y
+sudo npm install npm@latest -g
+sudo npm install n -g
+sudo n latest
+
 export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
 echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
@@ -13,4 +17,5 @@ sudo apt-get install mecab libmecab-dev mecab-ipadic-utf8
 git clone https://github.com/yamagame/AIRobotSamples
 cd AIRobotSamples
 npm i
-rpi-update
+
+sudo rpi-update
