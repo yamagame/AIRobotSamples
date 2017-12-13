e5e2c16f4a59b3615deb8d2c4600c513f5289064
Hiromitsu Yamaguchi <yamagame@mac.com>
2017-12-13 18:01:56 +0900

    形態素解析のコードに機能追加

--- mecab-mean.js
+++ mecab-mean.js
@@ -3,21 +3,126 @@ const sentense = process.argv[2] || "こんにちは";
 const talk = require('./talk');
 
 const utters = [
-  '(subject)^知る~テル(last)=',
-  '(subject)$ヲ^知る~テル(last)=',
-  '(subject)^知る~テル(last)=',
-  '(subject)^わかる(last)=',
-  '(subject)$ヲ^教える$テ(last)=',
-  '(subject)^教える$テ(last)=',
-  '(subject)$ハ(last)=',
+  [
+    'ask',
+    [
+      '[天気]知ってる',
+      '[天気]わかる',
+      '[天気]教えて',
+    ],
+  ],
+  [
+    'call',
+    [
+      '[ロボット君]聞こえてる',
+      '[ロボット君]聞こえる',
+    ],
+  ]
 ]
 
-mecab_parser(sentense, (err, result) => {
-  for (i=0;i<utters.length;i++) {
-    var ret = result.match(utters[i]);
-    if (ret) {
-      talk.play(`${ret.subject.net.text.join('')}を知りたい`);
-      break;
+//名詞だけを抜き出し
+function joinNoun(a)
+{
+  var r = {
+    net: [],
+    form: [],
+    text: [],
+  };
+  a.form.forEach( (v,i) => {
+    if (v[0] == '@') {
+      r.net.push(a.net[i]);
+      r.form.push(a.form[i]);
+      r.text.push(a.text[i]);
     }
+  })
+  return r;
+}
+
+function parseMatch(sentense, callback) {
+  mecab_parser(sentense, (err, result) => {
+    const r = {
+      net:[],
+      form:[],
+      text:[],
+    }
+    var step = 0;
+    result.net.form.forEach( (v, i) => {
+      switch(step) {
+        case 0:
+          if (v == '@[') {
+            step = 1;
+          } else
+          if (v == '=') {
+            r.net.push([])
+            r.form.push('(last)')
+            r.text.push('...')
+            r.net.push(result.net.net[i]);
+            r.form.push(result.net.form[i]);
+            r.text.push(result.net.text[i]);
+          } else {
+            r.net.push(result.net.net[i]);
+            r.form.push(result.net.form[i]);
+            r.text.push(result.net.text[i]);
+          }
+        case 1:
+          if (v == '@]') {
+            r.net.push([])
+            r.form.push('(subject)')
+            r.text.push('...')
+            step = 0;
+          } else {
+          }
+          break;
+      }
+    })
+    callback(null, r.form.join(''))
+  });
+}
+
+function matchSubject(utterList, result, callback) {
+  var n=0;
+  function nextUtter() {
+    if (n >= utterList.length) {
+      callback(null, null, null);
+      return;
+    }
+    const key = utterList[n][0];
+    const utters = utterList[n][1];
+    n++;
+    var c=0;
+    function _matchSubject(key) {
+      if (c >= utters.length) {
+        callback(null, null, null);
+        nextUtter();
+        return;
+      }
+      const utter = utters[c];
+      c++;
+      parseMatch(utter, (err, utter) => {
+        var ret = result.match(utter);
+        if (ret) {
+          callback(null, key, ret);
+          return;
+        }
+        _matchSubject(key);
+      });
+    }
+    _matchSubject(key);
   }
+  nextUtter();
+}
+
+mecab_parser(sentense, (err, result) => {
+// console.log(result);
+  matchSubject(utters, result, (err, key, ret) => {
+    if (ret) {
+      var noun = joinNoun(ret.subject.net).text.join('');
+      if (key == 'ask') {
+        talk.play(`${noun}を知りたい`);
+      } else {
+        talk.play(`${noun}を呼び出し`);
+      }
+      return;
+    }
+  });
 });
