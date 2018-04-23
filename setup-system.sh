835c4ab28f0d48aec59322f9d147198eb495260f
Hiromitsu Yamaguchi <yamagame@mac.com>
2018-04-23 12:27:33 +0900

    setupスクリプトからnodejsを分離

--- setup-system.sh
+++ setup-system.sh
@@ -3,21 +3,20 @@ cd ~
 
 sudo apt-get update
 sudo apt-get upgrade -y
-sudo apt-get install npm -y
-sudo npm install n -g
-sudo n lts
-sudo npm install npm@latest -g
+
+#sudo apt-get install npm -y
+#sudo npm install n -g
+#sudo n lts
+#sudo npm install npm@latest -g
 
 export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
 echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 sudo apt-get update && sudo apt-get install google-cloud-sdk -y
-git clone https://github.com/yamagame/AIRobotSamples
-cd AIRobotSamples
-npm i
 
-# Raspberr Pi Zero Wの場合は，以下のコメントアウトを外すこと
-# npm rebuild --build-from-source grpc
+#git clone https://github.com/yamagame/AIRobotSamples
+#cd AIRobotSamples
+#npm i
 
 sudo apt-get purge wolfram-engine -y
 sudo apt-get install mecab libmecab-dev mecab-ipadic-utf8 -y
@@ -29,4 +28,4 @@ sudo apt-get install ibus-anthy -y
 # pip3 install python3-xlib
 # pip3 install pyautogui
 
-sudo rpi-update
+# sudo rpi-update
